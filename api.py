116e9b3702ba8904b13cfe3d619e04a9e0f0d007
Jefferson Heard <jeff@terrahub.io>
2014-02-26 20:12:50 +0000

    Initial stubs for tastypie API

---
+++ api.py
@@ -0,0 +1,23 @@
+from django.contrib.auth.models import User
+
+from tastypie.authorization import Authorization
+from tastypie.resources import ModelResource
+from tastypie.api import Api
+
+from models import GenericResource
+
+
+class UserResource(ModelResource):
+    class Meta:
+        queryset = User.objects.all()
+        resource_name = 'user'
+        excludes = ['email', 'password', 'is_active', 'is_staff', 'is_superuser']
+        authorization = Authorization()
+
+class GenericResourceResource(ModelResource):
+    class Meta:
+        queryset = GenericResource.objects.all()
+        resource_name = 'resource'
+        authorization = Authorization()
+
+
