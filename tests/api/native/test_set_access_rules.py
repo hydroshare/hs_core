47435e0caa2400a7dda5b886161bcb11a85327fe
Lisa Stillwell <lisa@renci.org>
2014-05-06 12:57:45 -0400

    add test for set_access_rules

---
+++ tests/api/native/test_set_access_rules.py
@@ -0,0 +1,51 @@
+__author__ = 'lisa stillwell'
+
+from unittest import TestCase
+from hs_core.hydroshare import resource
+from hs_core.hydroshare import users
+from hs_core.models import GenericResource
+from django.contrib.auth.models import User
+import datetime as dt
+
+
+class TestSetAccessRules(TestCase):
+
+    def setUp(self):
+
+        # create a user
+        self.user1 = users.create_account(
+            'lisa@renci.org',
+            username='testuser1',
+            first_name='Lisa',
+            last_name='Stillwell',
+            superuser=False,
+            groups=[])
+
+        new_res = resource.create_resource(
+            'GenericResource',
+            self.user,
+            'My Test Resource'
+            )
+
+        self.res_id = new_res.short_id
+
+        # get the user's id
+        #self.userid = User.objects.get(username=self.user).pk
+
+        #self.group = users.create_group(
+            #'MyGroup',
+            #members=[self.user],
+            #owners=[self.user]
+            #)
+
+    def tearDown(self):
+        self.user1.delete()
+        #self.group.delete()
+        #self.res.delete()
+
+    def test_set_access_rules(self):
+
+
+        # get the resource by pid
+        res = resource.get_resource(self.res_id)
+
