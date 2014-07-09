2e400b7700955ac724b649a155b4418b539334b7
Jefferson Heard <jeff@dhcp152-54-9-202.europa.renci.org>
2014-07-09 11:03:30 -0400

    made IRODS optional with USE_IRODS setting in settings.py

--- models.py
+++ models.py
@@ -16,6 +16,7 @@ import os.path
 from django_irods.storage import IrodsStorage
 # from dublincore.models import QualifiedDublinCoreElement
 from dublincore import models as dc
+from django.conf import settings
 
 
 class GroupOwnership(models.Model):
@@ -216,14 +217,14 @@ class ResourceFile(models.Model):
     content_type = models.ForeignKey(ContentType)
 
     content_object = generic.GenericForeignKey('content_type', 'object_id')
-    resource_file = models.FileField(upload_to=get_path, storage=IrodsStorage())
+    resource_file = models.FileField(upload_to=get_path, storage=IrodsStorage() if settings.get('USE_IRODS', False) else settings.DEFAULT_FILE_STORAGE)
 
 class Bags(models.Model):
     object_id = models.PositiveIntegerField()
     content_type = models.ForeignKey(ContentType)
 
     content_object = generic.GenericForeignKey('content_type', 'object_id')
-    bag = models.FileField(upload_to='bags', storage=IrodsStorage(), null=True) # actually never null
+    bag = models.FileField(upload_to='bags', storage=IrodsStorage() if settings.get('USE_IRODS', False) else settings.DEFAULT_FILE_STORAGE, null=True) # actually never null
     timestamp = models.DateTimeField(default=now, db_index=True)
 
     class Meta:
