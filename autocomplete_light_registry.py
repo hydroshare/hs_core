310ea24517b87e654efc688ded026ff70ca1064f
Jefferson Heard <jeff@renci.org>
2014-07-26 15:10:42 +0000

    switched to email address for label

--- autocomplete_light_registry.py
+++ autocomplete_light_registry.py
@@ -1,8 +1,13 @@
 import autocomplete_light
 from django.contrib.auth.models import User, Group
 
-autocomplete_light.register(User,
-    search_fields=['username','email','first_name','last_name'])
+class UserAutocomplete(autocomplete_light.AutocompleteModelBase):
+    search_fields=['username','email','first_name','last_name']
+    
+    def choice_label(self, choice):
+        return choice.email
+
+autocomplete_light.register(User, UserAutocomplete)
 
 autocomplete_light.register(Group,
     search_fields=['name'])
